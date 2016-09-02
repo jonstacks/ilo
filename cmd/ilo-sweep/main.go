524038f10c716ec5aa3b6f98e11806bc0a55898e
Jonathan Stacks <jonstacks13@gmail.com>
2016-09-02 01:24:58 -0500

    Make a dummy sweep handler

--- cmd/ilo-sweep/main.go
+++ cmd/ilo-sweep/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	cidr := os.Args[1]
+
+	ip, ip_net, err := net.ParseCIDR(cidr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(7)
+	}
+	fmt.Printf("Sweeping %s for ILO servers\n", ip_net)
+	fmt.Printf("IP given: %s\n", ip)
 }
